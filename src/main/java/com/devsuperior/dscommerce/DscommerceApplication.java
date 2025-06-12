c210452c797bde9f5e9a58cd472176f7151e9392
Luan M Paes <luanmp25@gmail.com>
2025-06-12 20:56:19 -0300

    Order

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -8,6 +8,7 @@ public class DscommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
+		System.setProperty("spring.h2.console.enabled", "true");
 	}
 
 }
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,10 @@ public class User {
 	private LocalDate birthdate;
 	private String password;
 
+	@OneToMany(mappedBy = "client")
+	private List<Order>orders = new ArrayList<>();
+	
+	
 	public User() {
 
 	}
@@ -82,6 +89,10 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 }
