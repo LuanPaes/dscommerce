bc8d35966852c2351d6fab20efedac34a53e36f3
Luan M Paes <luanmp25@gmail.com>
2025-07-03 22:23:44 -0300

    Bean Validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,11 +2,23 @@ package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 
 	private Long id;
+	
+	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+	@NotBlank(message = "Campo Requerido")
 	private String name;
+	
+	@Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
+	@NotBlank(message = "Campo Requerido")
 	private String description;
+	
+	@Positive(message = "O preço deve ser positivo")
 	private double price;
 	private String imgUrl;
 
@@ -18,13 +30,17 @@ public class ProductDTO {
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
-
-	public ProductDTO() {
-
-
-	}
+	
 	public ProductDTO(Product product) {
-
+	       this.id = product.getId();
+	        this.name = product.getName();
+	        this.description = product.getDescription();
+	        this.price = product.getPrice();
+	        this.imgUrl = product.getImgUrl();
+	    }
+	
+	public ProductDTO() {
+		
 	}
 
 	public Long getId() {
